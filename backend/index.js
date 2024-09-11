6117f504c41dcca432eb1a73e7e4f336792cce80
Kanad2004 <kanadkulkarni2013@gmail.com>
2024-09-12 00:03:38 +0530

    First commit

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const dotenv = require("dotenv");
+const pinRoute = require("./routes/pin");
+dotenv.config();
+
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Database connected successfully");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use("/api/pin", pinRoute);
+
+app.listen(8000, () => {
+  console.log("Backend server is running on port 8000");
+});
